d2b74602aa982a45c8d95e53864fa1059ee33aa6
Aadit B <62301974+abennur33@users.noreply.github.com>
2023-09-07 21:01:15 -0400

    Update app.py

--- PassCraft/app.py
+++ PassCraft/app.py
@@ -263,7 +263,14 @@ def generate_membership_cards_and_save_locally(template_file_path, spreadsheet_f
     
     for index, row in df.iterrows():
         member_name = row['Name']
-        first_name, last_name = member_name.split()  # Split name into first and last name
+        names = member_name.split()
+        if len(names) >= 2:
+            first_name = names[0]
+            last_name = names[-1]
+        else:
+            # Handle the case where there's only one name (e.g., John)
+            first_name = member_name
+            last_name = ""  # Split name into first and last name  # Split name into first and last name
 
         output_file = f"{membership_card_dir}/MembershipCard_{member_name}.pdf"
         
